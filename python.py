269904d35aec987d1059bf7386e7bc3dc5f0bb27
Bond-z <31430102+Bond-z@users.noreply.github.com>
2023-04-12 11:44:45 +0700

    Create python.py

---
+++ python.py
@@ -0,0 +1,66 @@
+import xml.etree.ElementTree as ET
+import datetime
+import time
+import pandas
+import os
+from dotenv import load_dotenv
+
+resource_path = '../test_result/output.xml'
+
+date_time = datetime.datetime.now()
+
+tree = ET.parse(resource_path)
+root = tree.getroot()
+
+def get_TestCaseName():
+
+    TestCaseName = [] 
+    
+    for item in root[0].findall('test'):
+        
+        title = item.attrib["name"]
+        TestCaseName.append(title)
+
+    return TestCaseName
+
+def get_Result():
+    testcaseId = []
+    result = []
+
+    for item in root.findall('./statistics/tag/stat'):
+        
+        testcase_id = item.text
+        status_pass = item.attrib["pass"]
+        status_fail = item.attrib["fail"]
+
+        try:
+            if status_pass == "1":
+                testcaseId.append(testcase_id)
+                result.append("pass")
+          
+            elif status_fail == "1":
+                testcaseId.append(testcase_id)
+                result.append("fail")
+                
+        except Exception as e:
+            print('Error: ', str(e))
+    
+    df = pandas.DataFrame(testcaseId, columns=['ID'])
+    test_result = df.assign(Results = result)        
+
+    return test_result
+
+
+if __name__ == '__main__':
+
+    csv_file = 'report_' + str(date_time.strftime('%Y_%m_%d_%H_%M_%S')) + '.csv'
+    
+    testcase = get_TestCaseName()
+    result = get_Result()
+
+    print(result)
+    time.sleep(1)
+    result.insert(1, "Tese Cases", testcase, allow_duplicates=True)
+    result.sort_values(by=['ID'], axis=0, ascending=True)
+    result.to_csv(csv_file)
+    
